6195135215e84fc9278e0b3abfa6d1f0a4ad2ffd
Hariharasudhan-tech <77536865+Hariharasudhan-tech@users.noreply.github.com>
2022-07-25 20:00:39 +0530

    Add files via upload

---
+++ swappingTwoNumbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<< "Enter the first number: ";
+    cin >> a;
+    cout<< "Enter the second number: ";
+    cin >> b;
+    /*temp = num1;
+    num1 = num2;
+    num2 = temp;*/
+
+    /*a = a^b;
+    b = a^b;
+    a = a^b;*/
+
+    swap(a, b);
+    cout<<a<< " " <<b<<endl;
+
+
+
+
+
+
+
+    return 0;
+
+}
